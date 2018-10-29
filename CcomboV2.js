824c827884f20bf89e3569c36f98f5d67dde6946
sy-data <40491001+sy-data@users.noreply.github.com>
2018-10-29 10:12:37 +0900

    Update CcomboV2.js

--- CcomboV2.js
+++ CcomboV2.js
@@ -467,7 +467,7 @@ function setCascade5Init (index1, index2, index3, index4, index5, type1, type2, 
 }
 
 
-//(function (){
+(function (){
 	var fw = (typeof getFormWarpRequest == "function" ? getFormWarpRequest() : document.forms["formWarpRequest"]);
 	
 	let flag = false;
@@ -505,4 +505,4 @@ function setCascade5Init (index1, index2, index3, index4, index5, type1, type2, 
 			};
 	}
 	
-//})();
+})();
